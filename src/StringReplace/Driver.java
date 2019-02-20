08940ccae413f4ae7ec8d859060b51df0c04a6c6
Dhinesh <dhinesh.rbabu@gmail.com>
2019-02-20 10:20:47 +0530

    Replacing SubString in String 20/2/2019

---
+++ src/StringReplace/Driver.java
@@ -0,0 +1,9 @@
+package StringReplace;
+
+public class Driver {
+        public static void main(String[] args) {
+            String in = "ABIABAMABFINE";
+            in = in.replace("AB"," ");
+            System.out.println(in);
+        }
+}
